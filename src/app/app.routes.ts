608f9a7740d9ebb1a612ef418dc91ad6b1963c7f
Sherin Dharmarajan <sherinkd@live.com>
2017-07-06 10:38:27 -0400

    Added Dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from 'app/dashboard/dashboard.component';
 
 export const APP_ROUTES: Routes = [
+     { path: 'dashboard', component:  DashboardComponent },
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
