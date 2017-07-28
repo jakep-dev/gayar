9cbb270a7e40d6987cc777a65a0a547b01e6554d
Sherin Dharmarajan <sherinkd@live.com>
2017-07-27 21:22:05 -0400

    Added Unauthorized error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,9 @@ import { AuthRouteActivatorService } from './services/services';
 import { E401Component } from './shared/shared';
 
 export const APP_ROUTES: Routes = [
-    { path: 'dashboard',    component:  DashboardComponent},
-    { path: 'benchmark',    component:  BenchmarkComponent},
-    { path: 'search',       component:  SearchComponent, canActivate: [ AuthRouteActivatorService ]  },
+    { path: 'dashboard',    component:  DashboardComponent, canActivate: [ AuthRouteActivatorService ]},
+    { path: 'benchmark',    component:  BenchmarkComponent, canActivate: [ AuthRouteActivatorService ]},
+    { path: 'search',       component:  SearchComponent,    canActivate: [ AuthRouteActivatorService ]  },
     { path: 'sso/:userId',  component:  SsoComponent },
-    { path: '401',          component:  E401Component}
+    { path: '401',          component:  E401Component }
 ];
--- src/app/shared/errors/e-401/e-401.component.ts
+++ src/app/shared/errors/e-401/e-401.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from 'app/services/services';
 
 @Component({
   selector: 'app-e-401',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class E401Component implements OnInit {
 
-  constructor() { }
+  constructor(private menuService: MenuService) { }
 
   ngOnInit() {
+    this.menuService.isFullScreen = true;
   }
 
 }
