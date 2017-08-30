c9f37d0b538ead043fd6eabc13e9533425caa16b
Sherin Dharmarajan <sherinkd@live.com>
2017-08-29 21:43:58 -0400

    Resolved the paste on textbox

--- src/app/shared/constraints/number.directive.ts
+++ src/app/shared/constraints/number.directive.ts
@@ -4,19 +4,26 @@ import { Directive, HostListener } from '@angular/core';
   selector: '[restrictInputNumber]'
 })
 export class NumberDirective {
+  //Determine only digits
   validNumRegExp: any = /^\d+$/;
 
+  /**
+   * Event emits on keypress
+   * @param event 
+   */
   @HostListener('keypress', ['$event']) onKeyPressHandler(event: KeyboardEvent) {
     return (event.keyCode >=48 && event.keyCode <= 57);
   }
 
+  /**
+   * Event emits on textbox input
+   * @param event 
+   */
   @HostListener('input', ['$event']) onPasteHandler(event: KeyboardEvent) {
     let input = event.target as HTMLInputElement;
     let validNumberRegExp = new RegExp(this.validNumRegExp);
-    console.log(input.value);
-    let filteredValue = input.value.replace(/\D/g, '');
-    console.log(filteredValue);
     if(!validNumberRegExp.test(input.value)){
+      let filteredValue = input.value.replace(/\D/g, '');
       input.value = filteredValue || '';
     }
     return true;
