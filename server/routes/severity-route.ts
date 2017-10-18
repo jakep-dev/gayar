b0ab8425c072accfcc918fad0ac5d104432d8b18
Oneal Umusig <onealumusig@gmail.com>
2017-10-18 13:06:32 +0800

    Implement Severity Data Table...

--- server/routes/severity-route.ts
+++ server/routes/severity-route.ts
@@ -11,5 +11,25 @@ export class SeverityRouter extends BaseRoute {
 
     //Initialize all the api call endpoints
     init() {
+        this.app.post('/api/getSeverityData', this.getSeverityData);
     }
-}
+
+    public getSeverityData(req: Request, res: Response, next: NextFunction) {
+        try {
+            console.log('TOKEN: ' + req.body.token);
+            console.log('COMPANY ID: ' + req.body.company_id);
+            console.log('INDUSTRY: ' + req.body.industry);
+            console.log('REVENUE RANGE: ' + req.body.revenue_range);
+            super.PerformGetRequest("getSeverityData", {
+                'ssnid': req.body.token,
+                'company_id': req.body.company_id,
+                'industry': req.body.industry,
+                'revenue_range': req.body.revenue_range
+            }, (data) => {
+                res.send(data);
+            });
+        }
+        catch (e) {
+            Logger.error(e);
+        }
+    }}
--- src/app/model/severity.model.ts
+++ src/app/model/severity.model.ts
@@ -4,3 +4,17 @@ export interface SeverityInput {
     naics: string;
     revenueRange: string;
 }
+
+export interface SeverityDataResponseModel {
+    peerGroup: Array<SeverityDataModel>,
+    company: Array<SeverityDataModel>
+}
+
+export interface SeverityDataModel {
+    company_name: string;
+    type_of_incident: string;
+    incident_date: string;
+    records_affected: string;
+    type_of_loss: string;
+    case_description: string;
+}
