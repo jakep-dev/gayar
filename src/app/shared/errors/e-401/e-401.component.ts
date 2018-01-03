7d23c66a2aea828bc28474841da68cb030738dd4
Sherin Dharmarajan <sherinkd@live.com>
2018-01-03 00:09:44 -0500

    Resolved the error message

--- src/app/shared/errors/e-401/e-401.component.ts
+++ src/app/shared/errors/e-401/e-401.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from 'app/services/services';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-e-401',
   templateUrl: './e-401.component.html',
-  styleUrls: ['./e-401.component.css']
+  styleUrls: ['./e-401.component.scss']
 })
 export class E401Component implements OnInit {
 
@@ -14,4 +15,12 @@ export class E401Component implements OnInit {
     this.menuService.isFullScreen = true;
   }
 
+  navigateToLogin () {
+    const loginUrl: string = environment.landingPage;
+    let winRef = window.open('', loginUrl, '', true);
+    if (winRef.location.href === 'about:blank'){
+      winRef.location.href = loginUrl;
+   }
+   winRef.location.href = loginUrl;
+  }
 }
